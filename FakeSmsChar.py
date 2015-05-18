1721bbabfb2a2e53a781533cb1f11f08c34f893d
Ilya Petrash <ip1992@inbox.ru>
2015-05-18 17:41:30 +0500

    Update FakeSmsChar.py

--- FakeSmsChar.py
+++ FakeSmsChar.py
@@ -22,7 +22,8 @@ __author__ = 'ipetrash'
 # и получаешь сгенерированную картинку
 # Потом сделать gui'шный вариант с предпросмотром
 
-
+# TODO: рисовать тени под облачками
+# TODO: поиграть с градиентами
 
 from PySide.QtGui import *
 from PySide.QtCore import *
@@ -34,36 +35,23 @@ class Sms:
         self.text = text
         self.time = time
 
-        # self.font = QFont('Tahoma', 10)
-        # self.font_time = QFont('Tahoma', 8)
-
     def get_sms_size(self, font):
         """Функция возвращает размеры облачка смс"""
 
         w, h = 0, 0
 
+        # Разбиваем строку, определяем максимальную ширину и
+        # подсчитываем высоту смс
         for line in self.text.split('\n'):
-            # TODO: доработать -- расстояние внутри между рамками
-            # облачка и текстом получается меньше, если многострочный
-            # текст смс
-            # text_rect = QFontMetrics(self.font).boundingRect(line)
             text_rect = QFontMetrics(font).boundingRect(line)
             w = max(w, text_rect.width())
-            h = max(h, text_rect.height())
+            h += text_rect.height()
 
-        # TODO: наверное, лучше будет увеличить процентно
-        return w + 20, h + 20
-
-        # text_rect = QFontMetrics(self.font).boundingRect(text)
-        #
-        # # Немного увеличим облачко, чтобы текст в нем вмещался свободно
-        # # TODO: наверное, лучше будет увеличить процентно
-        # return text_rect.width() + 20, text_rect.height() + 20
+        return w + 15, h + 15
 
     def get_time_sms_size(self, font):
         """Функция возвращает размеры текст с временем смс"""
 
-        # time_rect = QFontMetrics(self.font_time).boundingRect(self.time)
         time_rect = QFontMetrics(font).boundingRect(self.time)
         return time_rect.width(), time_rect.height()
 
@@ -76,20 +64,19 @@ class FakeSmsChat:
         self.other_color = Qt.gray
         self.text_color = Qt.black
         self.indent = 10
-        self.left_indent = 5
-        self.right_indent = 5
-        self.top_indent = 5
+        self.left_indent = 15
+        self.right_indent = 15
+        self.top_indent = 10
         self.indent_time = 5
         self.font_time = QFont('Tahoma', 8)
+
+        self.h_triangle = 10
+        self.w_triangle = 10
 
     def add_sms(self, me, text, time):
         sms = Sms(me, text, time)
 
         self.sms_chats.append(sms)
-
-        # self.sms_chats.append(
-        #     (me, text, time)
-        # )
 
     def add_me_sms(self, text, time=None):
         self.add_sms(True, text, time)
@@ -97,43 +84,16 @@ class FakeSmsChat:
     def add_other_sms(self, text, time=None):
         self.add_sms(False, text, time)
 
-    # def get_sms_size(self, text):
-    #     """Функция возвращает размеры облачка смс"""
-    #
-    #     w, h = 0, 0
-    #
-    #     for line in text.split('\n'):
-    #         text_rect = QFontMetrics(self.font).boundingRect(line)
-    #         w = max(w, text_rect.width())
-    #         h = max(h, text_rect.height())
-    #
-    #     # TODO: наверное, лучше будет увеличить процентно
-    #     return w + 20, h + 20
-    #
-    #     # text_rect = QFontMetrics(self.font).boundingRect(text)
-    #     #
-    #     # # Немного увеличим облачко, чтобы текст в нем вмещался свободно
-    #     # # TODO: наверное, лучше будет увеличить процентно
-    #     # return text_rect.width() + 20, text_rect.height() + 20
-    #
-    # def get_time_sms_size(self, time):
-    #     """Функция возвращает размеры текст с временем смс"""
-    #
-    #     time_rect = QFontMetrics(self.font_time).boundingRect(time)
-    #     return time_rect.width(), time_rect.height()
-
     def size(self):
         """Функция для размера переписки в пикселях."""
         w, h = 0, 0
 
         for sms in self.sms_chats:
-            # w_sms, h_sms = self.get_sms_size(sms[1])
             w_sms, h_sms = sms.get_sms_size(self.font)
 
-            # TODO: учитывать размер времени sms[2]
-            # t_w = self.get_time_sms_size(sms[2])[0]
+            # Получаем ширину текста с временем отправки
             t_w = sms.get_time_sms_size(self.font_time)[0]
-            w_sms += t_w + self.indent_time
+            w_sms += t_w + self.indent_time + self.w_triangle * 2
 
             w = max(w, w_sms)
             h += h_sms
@@ -142,10 +102,48 @@ class FakeSmsChat:
         h += len(self.sms_chats) * self.indent
 
         # Немного добавим размера
-        w += 10
-        h += 10
+        return w + 40, h + 40
 
-        return w, h
+    def drawSmsCloud(self, x, y, w, h, painter, color, sms):
+        painter.setPen(color)
+        painter.setBrush(color)
+
+        path = QPainterPath()
+        path.setFillRule(Qt.WindingFill)
+        path.addRoundRect(x, y, w, h, 5, 5)
+
+        # Нарисуем треугольник сбоку от прямоугольника
+        if sms.me:
+            path.moveTo(x - self.w_triangle, y + h / 2)
+            path.lineTo(x, y + h / 2 - self.h_triangle / 2)
+            path.lineTo(x, y + h / 2 + self.h_triangle / 2)
+        else:
+            path.moveTo(x + w + self.w_triangle, y + h / 2)
+            path.lineTo(x + w, y + h / 2 - self.h_triangle / 2)
+            path.lineTo(x + w, y + h / 2 + self.h_triangle / 2)
+
+        path.closeSubpath()
+
+        painter.drawPath(path)
+
+    def drawSmsText(self, x, y, w, h, painter, color, sms):
+        painter.setFont(self.font)
+        painter.setPen(color)
+        painter.drawText(x, y, w, h, Qt.AlignCenter, sms.text)
+
+    def drawSmsTime(self, x, y, w, h, painter, sms):
+        time_w, time_h = sms.get_time_sms_size(self.font_time)
+
+        # Высчитываем горизонтальное положение текста с временем отправки смс
+        if sms.me:
+            # К горизонтальному положению смс добавляем его ширину и отступ текста времени отправки
+            time_x = x + w + self.indent_time
+        else:
+            # Из горизонтального положения смс вычитаем его ширину и отступ текста времени отправки
+            time_x = x - time_w - self.indent_time
+
+        painter.setFont(self.font_time)
+        painter.drawText(time_x, y, time_w, h, Qt.AlignCenter, sms.time)
 
     def get_QImage(self):
         """Отрисовка смс диалога на QImage"""
@@ -161,47 +159,20 @@ class FakeSmsChat:
         last_y = self.top_indent
 
         for sms in self.sms_chats:
-            # me = sms[0]
-            # message = sms[1]
-            # time = sms[2]
             me = sms.me
-            message = sms.text
-            time = sms.time
 
             # Цвет облачков собеседников должны отличаться
             sms_color = self.me_color if me else self.other_color
             text_color = self.text_color
 
-            # w, h = self.get_sms_size(message)
             w, h = sms.get_sms_size(self.font)
 
             # "Мои" смс будут находиться с левой стороны, а собеседника с правой стороны
             x = self.left_indent if me else w_im - w - self.right_indent
 
-            p.setPen(sms_color)
-            p.setBrush(sms_color)
-            p.setFont(self.font)
-            p.drawRoundedRect(x, last_y, w, h, 20, 20)
-
-            p.setPen(text_color)
-            p.drawText(x, last_y, w, h, Qt.AlignCenter, message)
-            # p.drawText(x, last_y, w, h, Qt.AlignCenter | Qt.TextWordWrap, message)
-
-            # TODO: refactoring
-            if time:
-                # time_w, time_h = self.get_time_sms_size(time)
-                time_w, time_h = sms.get_time_sms_size(self.font_time)
-
-                # Высчитываем горизонтальное положение текста с временем отправки смс
-                if me:
-                    # К горизонтальному положению смс добавляем его ширину и отступ текста времени отправки
-                    time_x = x + w + self.indent_time
-                else:
-                    # Из горизонтального положения смс вычитаем его ширину и отступ текста времени отправки
-                    time_x = x - time_w - self.indent_time
-
-                p.setFont(self.font_time)
-                p.drawText(time_x, last_y, time_w, h, Qt.AlignCenter, time)
+            self.drawSmsCloud(x, last_y, w, h, p, sms_color, sms)
+            self.drawSmsText(x, last_y, w, h, p, text_color, sms)
+            self.drawSmsTime(x, last_y, w, h, p, sms)
 
             last_y += h + self.indent
 
@@ -217,7 +188,7 @@ app = QApplication(sys.argv)
 fake = FakeSmsChat()
 fake.add_me_sms('Кек, а меня уволили.', '10:01')
 fake.add_other_sms('за что уже?', '10:02')
-fake.add_me_sms('За то что у кошки роды принимал.', '10:03')
+fake.add_me_sms('За то что у кошки\n роды принимал.', '10:03')
 fake.add_other_sms('ШТОООА7', '10:04')
 fake.add_other_sms('прям на работе?')
 fake.add_me_sms('Нет. Я один день не \nвышел на работу.')
