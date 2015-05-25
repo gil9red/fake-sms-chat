905c76b14bd2caa13b3160fe83b19f2a7bb83e4c
Ilya Petrash <ip1992@inbox.ru>
2015-05-25 12:07:47 +0500

    Create example.py

---
+++ example.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'ipetrash'
+
+from FakeSmsChat import *
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    fake = FakeSmsChat()
+    fake.add_me_sms('Кек, а меня уволили.', '10:01')
+    fake.add_other_sms('за что уже?', '10:02')
+    fake.add_me_sms('За то что у кошки\n роды принимал.', '10:03')
+    fake.add_other_sms('ШТОООА7', '10:04')
+    fake.add_other_sms('прям на работе?')
+    fake.add_me_sms('Нет. Я один день не \nвышел на работу.')
+    fake.add_other_sms('зато у тебя есть котики', '10:07')
+    fake.add_other_sms('котики - это хорошо')
+
+    im = fake.get_QImage()
+    im.save('fakesms.png')
